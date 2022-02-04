79ec48cd3d6f77ac3b75434c0503d2dad089bf7a
Adam Franco <afranco@middlebury.edu>
2022-02-04 13:38:00 -0500

    Filter/copy output properties to more closely mock OpenMapTiles schema.

--- samples/OpenMapTiles/HighwayAttributes.js
+++ samples/OpenMapTiles/HighwayAttributes.js
@@ -3,6 +3,8 @@
 import Sample from "../../lib/Sample.js";
 import Layer from "../../lib/Layer.js";
 import WayRowsSampleData from "../../lib/SampleData/WayRowsSampleData.js";
+import CopyPropertiesTransformation from "../../lib/Transformation/CopyPropertiesTransformation.js";
+import RemovePropertiesTransformation from "../../lib/Transformation/RemovePropertiesTransformation.js";
 
 var sample = new Sample(
   'highway-attributes',
@@ -14,41 +16,51 @@ var sample = new Sample(
 
 var sampleData = new WayRowsSampleData(
   [
-    { 'name:latin': 'motorway', 'class': 'motorway' },
-    { 'name:latin': 'trunk', 'class': 'trunk' },
-    { 'name:latin': 'trnk expy', 'class': 'trunk', 'expressway': 1},
-    { 'name:latin': 'primary', 'class': 'primary' },
-    { 'name:latin': 'pri expy', 'class': 'primary', 'expressway': 1},
-    { 'name:latin': 'secondary', 'class': 'secondary' },
-    { 'name:latin': 'sec expy', 'class': 'secondary', 'expressway': 1},
-    { 'name:latin': 'tertiary', 'class': 'tertiary' },
-    { 'name:latin': 'ter expy', 'class': 'tertiary', 'expressway': 1},
-    { 'name:latin': 'minor', 'class': 'minor' },
-    { 'name:latin': 'service', 'class': 'service' },
-    { 'name:latin': 'driveway', 'class': 'service', 'service': 'driveway' },
-    { 'name:latin': 'alley', 'class': 'service', 'service': 'alley' },
-    { 'name:latin': 'parking_aisle', 'class': 'service', 'service': 'parking_aisle' },
-    { 'name:latin': 'raceway', 'class': 'raceway' },
-    { 'name:latin': 'track', 'class': 'track' },
-    { 'name:latin': 'path', 'class': 'path', 'subclass': 'path' },
-    { 'name:latin': 'cycleway', 'class': 'path', 'subclass': 'cycleway' },
-    { 'name:latin': 'bridleway', 'class': 'path', 'subclass': 'bridleway' },
-    { 'name:latin': 'footway', 'class': 'path', 'subclass': 'footway' },
-    { 'name:latin': 'steps', 'class': 'path', 'subclass': 'steps' },
+    { 'name': 'motorway', 'class': 'motorway' },
+    { 'name': 'trunk', 'class': 'trunk' },
+    { 'name': 'trnk expy', 'class': 'trunk', 'expressway': 1},
+    { 'name': 'primary', 'class': 'primary' },
+    { 'name': 'pri expy', 'class': 'primary', 'expressway': 1},
+    { 'name': 'secondary', 'class': 'secondary' },
+    { 'name': 'sec expy', 'class': 'secondary', 'expressway': 1},
+    { 'name': 'tertiary', 'class': 'tertiary' },
+    { 'name': 'ter expy', 'class': 'tertiary', 'expressway': 1},
+    { 'name': 'minor', 'class': 'minor' },
+    { 'name': 'service', 'class': 'service' },
+    { 'name': 'driveway', 'class': 'service', 'service': 'driveway' },
+    { 'name': 'alley', 'class': 'service', 'service': 'alley' },
+    { 'name': 'parking_aisle', 'class': 'service', 'service': 'parking_aisle' },
+    { 'name': 'raceway', 'class': 'raceway' },
+    { 'name': 'track', 'class': 'track' },
+    { 'name': 'path', 'class': 'path', 'subclass': 'path' },
+    { 'name': 'cycleway', 'class': 'path', 'subclass': 'cycleway' },
+    { 'name': 'bridleway', 'class': 'path', 'subclass': 'bridleway' },
+    { 'name': 'footway', 'class': 'path', 'subclass': 'footway' },
+    { 'name': 'steps', 'class': 'path', 'subclass': 'steps' },
   ],
   [
     {},
-    { 'name:latin': 'bridge', 'brunnel': 'bridge', 'layer': 1 },
-    { 'name:latin': 'tunnel', 'brunnel': 'tunnel', 'layer': -1 },
-    { 'name:latin': 'ford', 'brunnel': 'ford' },
-    { 'name:latin': 'oneway', 'oneway': 1 },
-    { 'name:latin': 'paved', 'surface': 'paved' },
-    { 'name:latin': 'unpaved', 'surface': 'unpaved' },
+    { 'name': 'bridge', 'brunnel': 'bridge', 'layer': 1 },
+    { 'name': 'tunnel', 'brunnel': 'tunnel', 'layer': -1 },
+    { 'name': 'ford', 'brunnel': 'ford' },
+    { 'name': 'oneway', 'oneway': 1 },
+    { 'name': 'paved', 'surface': 'paved' },
+    { 'name': 'unpaved', 'surface': 'unpaved' },
   ],
   [-100, 40]
 );
-sample.addLayer(new Layer('transportation')).addSampleData(sampleData);
-sample.addLayer(new Layer('transportation_name')).addSampleData(sampleData);
+sample.addLayer(new Layer('transportation')).addSampleData(
+  new RemovePropertiesTransformation(sampleData, ['name'])
+);
+sample.addLayer(new Layer('transportation_name')).addSampleData(
+  new CopyPropertiesTransformation(
+    new RemovePropertiesTransformation(
+      sampleData,
+      ['brunnel', 'layer', 'surface', 'expressway', 'oneway']
+    ),
+    { 'name': ['name_en', 'name_de', 'name_int', 'name:latin'] }
+  )
+);
 
 for (var i = 4; i <= 20; i = i + 1) {
   sample.setZoomVariant(i);
